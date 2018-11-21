9eabac1714fc68a9eb83fce060765c3463e41f78
zhangyaping <916459426@qq.com>
2018-11-22 01:07:11 +0800

    commit

--- src/main/java/com/netty/study/fifth/websocket/MyServer.java
+++ src/main/java/com/netty/study/fifth/websocket/MyServer.java
@@ -5,6 +5,8 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * Created by zhangyaping on 18/11/20.
@@ -18,7 +20,8 @@ public class MyServer {
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                    .childHandler(null);
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketInitializer());
 
             ChannelFuture channelFuture = bootstrap.bind(8899).sync();
             channelFuture.channel().closeFuture().sync();
--- src/main/java/com/netty/study/fifth/websocket/WebSocketHandler.java
+++ src/main/java/com/netty/study/fifth/websocket/WebSocketHandler.java
@@ -2,15 +2,37 @@ package com.netty.study.fifth.websocket;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by zhangyaping on 18/11/21.
  */
-public class WebSocketHandler extends SimpleChannelInboundHandler<String> {
+public class WebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("收到消息:" + msg.text());
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间:" + LocalDateTime.now()));
 
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded:" + ctx.channel().id().asLongText());
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved: " + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("exception");
+        ctx.close();
     }
 }
