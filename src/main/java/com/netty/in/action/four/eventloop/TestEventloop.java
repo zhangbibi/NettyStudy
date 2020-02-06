aad4de651c207ab2a4528e8d100ec394aeae6cdf
zhangyaping <916459426@qq.com>
2020-02-06 13:44:38 +0800

    commit

--- src/main/java/com/netty/in/action/four/eventloop/TestEventloop.java
+++ src/main/java/com/netty/in/action/four/eventloop/TestEventloop.java
@@ -4,7 +4,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
-import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
